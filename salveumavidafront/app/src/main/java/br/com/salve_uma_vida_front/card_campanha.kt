0f63b5e562790c41a2d7d67bdc978818208340e1
BrotherPool <marcoslevi1213@gmail.com>
2020-08-05 19:57:06 -0300

    Commit inicial Criação do protótipo do card da campanha

---
+++ salveumavidafront/app/src/main/java/br/com/salve_uma_vida_front/card_campanha.kt
@@ -0,0 +1,11 @@
+package br.com.salve_uma_vida_front
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class card_campanha : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_campanha)
+    }
+}
