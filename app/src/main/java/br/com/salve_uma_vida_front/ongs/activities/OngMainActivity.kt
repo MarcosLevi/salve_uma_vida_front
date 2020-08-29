83f453fa1195c382f1a4b4f9fc1c9697393bef5c
MarcosLevi <marcoslevi1213@gmail.com>
2020-08-29 14:30:32 -0300

    Na activity ong, quando existe a mudança de fragment, o título também muda

--- app/src/main/java/br/com/salve_uma_vida_front/ongs/activities/OngMainActivity.kt
+++ app/src/main/java/br/com/salve_uma_vida_front/ongs/activities/OngMainActivity.kt
@@ -3,6 +3,8 @@ package br.com.salve_uma_vida_front.ongs.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import br.com.salve_uma_vida_front.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -13,6 +15,9 @@ class OngMainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main_ong_navigation)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view_ong)
         val navController = findNavController(R.id.nav_host_fragment_ong)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.procurarFragment, R.id.favoritosFragment, R.id.campanhasFragment, R.id.mensagensFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
     }
 }
