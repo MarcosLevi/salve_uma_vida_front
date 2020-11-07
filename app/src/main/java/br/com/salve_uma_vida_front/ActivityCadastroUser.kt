4fc41d6d82ad90a2fa03ebf5b9f92b4f9860f7bb
MarcosLevi <marcoslevi1213@gmail.com>
2020-11-07 18:43:37 -0300

    Geocoder pegando endereço e transformando em latitude e longitude e validação dos campos de cadastro

--- app/src/main/java/br/com/salve_uma_vida_front/ActivityCadastroUser.kt
+++ app/src/main/java/br/com/salve_uma_vida_front/ActivityCadastroUser.kt
@@ -1,15 +1,19 @@
 package br.com.salve_uma_vida_front
 
+import android.location.Geocoder
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
+import br.com.salve_uma_vida_front.both.AdressToLatLong
 import br.com.salve_uma_vida_front.both.dp
 import br.com.salve_uma_vida_front.databinding.ActivityCadastroUserBinding
 import br.com.salve_uma_vida_front.dto.FiltroPesquisaDto
 import com.squareup.picasso.Picasso
+import java.util.*
 
 class ActivityCadastroUser : AppCompatActivity() {
 
@@ -24,17 +28,32 @@ class ActivityCadastroUser : AppCompatActivity() {
 
             val radio = binding.root.findViewById<RadioButton>(selectedId)
             if (radio == binding.UserOng) {
-                binding.labelEndereco.visibility = View.VISIBLE
-                binding.endereco.visibility = View.VISIBLE
+                binding.labelRua.visibility = View.VISIBLE
+                binding.rua.visibility = View.VISIBLE
+                binding.labelCidade.visibility = View.VISIBLE
+                binding.cidade.visibility = View.VISIBLE
+                binding.labelEstado.visibility = View.VISIBLE
+                binding.estado.visibility = View.VISIBLE
+                binding.labelNumero.visibility = View.VISIBLE
+                binding.numero.visibility = View.VISIBLE
             } else {
-                binding.labelEndereco.visibility = View.INVISIBLE
-                binding.endereco.visibility = View.INVISIBLE
-                binding.endereco.text.clear()
+                binding.labelRua.visibility = View.GONE
+                binding.rua.visibility = View.GONE
+                binding.rua.text.clear()
+                binding.labelCidade.visibility = View.GONE
+                binding.cidade.visibility = View.GONE
+                binding.cidade.text.clear()
+                binding.labelEstado.visibility = View.GONE
+                binding.estado.visibility = View.GONE
+                binding.estado.text.clear()
+                binding.labelNumero.visibility = View.GONE
+                binding.numero.visibility = View.GONE
+                binding.numero.text.clear()
             }
 
         }
-        binding.imagem.setOnFocusChangeListener(View.OnFocusChangeListener{view, hasFocus ->
-            if (!hasFocus){
+        binding.imagem.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
                 Picasso.get()
                     .load(binding.imagem.text.toString())
                     .resize(110.dp, 110.dp)
@@ -44,12 +63,71 @@ class ActivityCadastroUser : AppCompatActivity() {
                     .into(binding.userFoto)
             }
 
-        })
+        }
+        binding.criar.setOnClickListener {
+            if (validade()) {
+                if (getRadioSelected() == binding.UserOng) {
+                    var adressToLatLong =
+                        AdressToLatLong(getEnderecoFormatado(), applicationContext)
+                    var latitude = adressToLatLong.get(0)
+                    var longitude = adressToLatLong.get(1)
+                }
+            }
 
+
+        }
+    }
+
+    private fun getEnderecoFormatado(): String {
+        return binding.rua.text.toString() + ", " + binding.cidade.text.toString() + " - " + binding.estado.text.toString() + ", " + binding.numero.text.toString()
     }
 
     private fun getRadioSelected(): RadioButton? {
         val selectedId = binding.radioGroupTipo.checkedRadioButtonId
         return binding.root.findViewById(selectedId)
     }
+
+    private fun validade(): Boolean {
+        if (TextUtils.isEmpty(binding.imagem.text)) {
+            binding.imagem.setError("Imagem é necessária para fazer o cadastro")
+            return false
+        }
+        if (TextUtils.isEmpty(binding.nome.text)) {
+            binding.nome.setError("Nome é necessário para fazer o cadastro")
+            return false
+        }
+        if (TextUtils.isEmpty(binding.detalhes.text)) {
+            binding.detalhes.setError("Detalhes são necessários para fazer o cadastro")
+            return false
+        }
+        if (TextUtils.isEmpty(binding.email.text)) {
+            binding.email.setError("Email é necessário para fazer o cadastro")
+            return false
+        }
+        if (TextUtils.isEmpty(binding.senha.text)) {
+            binding.senha.setError("Senha é necessária para fazer o cadastro")
+            return false
+        }
+        if (getRadioSelected() == binding.UserOng) {
+            if (TextUtils.isEmpty(binding.rua.text)) {
+                binding.rua.setError("Rua/Avenida é necessária para fazer o cadastro")
+                return false
+            }
+            if (TextUtils.isEmpty(binding.cidade.text)) {
+                binding.cidade.setError("Cidade é necessária para fazer o cadastro")
+                return false
+            }
+            if (TextUtils.isEmpty(binding.estado.text)) {
+                binding.estado.setError("Estado é necessário para fazer o cadastro")
+                return false
+            }
+            if (TextUtils.isEmpty(binding.numero.text)) {
+                binding.numero.setError("Numero é necessário para fazer o cadastro")
+                return false
+            }
+        }
+        return true
+    }
+
+
 }
--- app/src/main/java/br/com/salve_uma_vida_front/both/funcoesAuxiliares.kt
+++ app/src/main/java/br/com/salve_uma_vida_front/both/funcoesAuxiliares.kt
@@ -3,10 +3,10 @@ package br.com.salve_uma_vida_front.both
 import android.app.Activity
 import android.content.Context
 import android.content.res.Resources
+import android.location.Geocoder
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 import java.util.*
 
 fun Context.hideKeyboard(view: View) {
@@ -36,5 +36,20 @@ fun FormatStringToDate(string: String): String{
     val formatter = SimpleDateFormat("dd/MMM/yyyy");
     return "Ocorrerá em "+formatter.format(date)
 }
+
+fun AdressToLatLong(adress: String, applicationContext: Context): MutableList<Double> {
+    val geocoder = Geocoder(applicationContext, Locale.getDefault())
+    var fromLocationName = geocoder.getFromLocationName(adress, 1)
+    var latitude = fromLocationName[0].latitude
+    var longitude = fromLocationName[0].longitude
+    return mutableListOf(latitude,longitude)
+}
+
+fun LatLongToAdress(latitude: Double,longitude:Double, applicationContext: Context): String {
+    val geocoder = Geocoder(applicationContext, Locale.getDefault())
+    var fromLocation = geocoder.getFromLocation(latitude, longitude, 1)
+    return fromLocation[0].getAddressLine(0)
+}
+
 val Int.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
