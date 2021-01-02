ef9bf85a8c47e916b5943c768e762bf49271ad74
MarcosLevi <marcoslevi1213@gmail.com>
2021-01-02 17:11:51 -0300

    Ajuste da tela de cadastro de campanha uso do inputLayout

--- app/src/main/java/br/com/salve_uma_vida_front/fragments/CadastroCampanhaFragment.kt
+++ app/src/main/java/br/com/salve_uma_vida_front/fragments/CadastroCampanhaFragment.kt
@@ -11,16 +11,14 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import br.com.salve_uma_vida_front.DateToString
-import br.com.salve_uma_vida_front.DateToStringBanco
-import br.com.salve_uma_vida_front.R
+import br.com.salve_uma_vida_front.*
 import br.com.salve_uma_vida_front.adapters.ItemAdapterOng
 import br.com.salve_uma_vida_front.databinding.FragmentCadastroCampanhaBinding
 import br.com.salve_uma_vida_front.dto.CampanhaDto
 import br.com.salve_uma_vida_front.dto.CampanhaItemDto
 import br.com.salve_uma_vida_front.models.DialogEditaItemNew
-import br.com.salve_uma_vida_front.toolbarVazia
 import br.com.salve_uma_vida_front.viewholders.itemCadastroCampanhaViewHolder
+import com.squareup.picasso.Picasso
 import java.util.*
 
 class CadastroCampanhaFragment : Fragment(), ItemAdapterOng.ItemListener,
@@ -54,8 +52,6 @@ class CadastroCampanhaFragment : Fragment(), ItemAdapterOng.ItemListener,
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
 
-        val calendar = Calendar.getInstance()
-
         atualizaQuantidadeDeItens()
 
         configuraRecyclerView()
@@ -68,28 +64,22 @@ class CadastroCampanhaFragment : Fragment(), ItemAdapterOng.ItemListener,
 //            .placeholder(R.drawable.ic_dafault_photo)
 //            .error(R.drawable.ic_baseline_report_problem_24)
 //            .into(binding.cadastroCampanhaImagem)
+        configuraImagemCampanha()
 
 
-        val adicionaItem = binding.cadastroCampanhaAdicionaItem
-        adicionaItem.setOnClickListener {
-            val dialogEditaItemNew = DialogEditaItemNew(this)
-            dialogEditaItemNew.show(parentFragmentManager, "Dialog edita item")
-        }
+        configuraAdicionarItem()
 
-        val finalizaCampanha = binding.cadastroCampanhaFinalizar
-        finalizaCampanha.setOnClickListener {
-//            if(validate()){
-//
-//            }
-            campanha.descricao = binding.cadastroCampanhaDescricao.text.toString()
-            campanha.titulo = binding.cadastroCampanhaTitulo.text.toString()
-//            viewModel.cadastraCampanha()
-        }
+        configuraFinalizarCampanha()
 
-        val dataCampanha = binding.cadastroCampanhaData
+        configuraDatePicker(view)
 
-        val labelDataCampanha = binding.cadastroCampanhaLabelData
+        configuraToolbar()
 
+    }
+
+    private fun configuraDatePicker(view: View) {
+        val dataCampanha = binding.cadastroEventoData
+        val calendar = Calendar.getInstance()
         val escolheData = binding.cadastroCampanhaPickDate
         escolheData.setOnClickListener {
             val dia = calendar.get(Calendar.DAY_OF_MONTH)
@@ -102,10 +92,7 @@ class CadastroCampanhaFragment : Fragment(), ItemAdapterOng.ItemListener,
                 DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                     // Display Selected date in TextView
                     calendar.set(year, monthOfYear, dayOfMonth)
-                    labelDataCampanha.visibility = View.VISIBLE
-                    dataCampanha.text = DateToString(
-                        calendar
-                    )
+                    dataCampanha.setText(DateToString(calendar))
                     campanha.data = DateToStringBanco(calendar)
                 },
                 ano,
@@ -113,17 +100,44 @@ class CadastroCampanhaFragment : Fragment(), ItemAdapterOng.ItemListener,
                 dia
             )
             dpd.show()
-
         }
+    }
 
-        configuraToolbar()
+    private fun configuraFinalizarCampanha() {
+        val finalizaCampanha = binding.cadastroCampanhaFinalizar
+        finalizaCampanha.setOnClickListener {
+    //            if(validate()){
+    //
+    //            }
+            campanha.descricao = binding.cadastroCampanhaDescricao.text.toString()
+            campanha.titulo = binding.cadastroCampanhaTitulo.text.toString()
+    //            viewModel.cadastraCampanha()
+        }
+    }
 
+    private fun configuraAdicionarItem() {
+        val adicionaItem = binding.cadastroCampanhaAdicionaItem
+        adicionaItem.setOnClickListener {
+            val dialogEditaItemNew = DialogEditaItemNew(this)
+            dialogEditaItemNew.show(parentFragmentManager, "Dialog edita item")
+        }
+    }
+
+    private fun configuraImagemCampanha() {
+        Picasso.get()
+            .load("https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg")
+            .resize(110.dp, 110.dp)
+            .centerCrop()
+            .placeholder(R.drawable.ic_dafault_photo)
+            .error(R.drawable.ic_baseline_report_problem_24)
+            .into(binding.cadastroCampanhaImagem)
+        campanha.userImage =
+            "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg"
     }
 
     private fun configuraToolbar() {
         val toolbar = toolbarVazia(activity)
         toolbar?.setBackgroundColor(resources.getColor(R.color.corCampanhas))
-//        toolbar?.inflateMenu(R.menu.fragment_both_procurar_menu)
     }
 
     private fun configuraRecyclerView() {
@@ -149,7 +163,7 @@ class CadastroCampanhaFragment : Fragment(), ItemAdapterOng.ItemListener,
     }
 
     override fun onEditaClicked(itemCampanha: CampanhaItemDto) {
-        isEdita=true
+        isEdita = true
         Log.d("CadastroCampanha", "Cliquei em editar")
         val dialogEditaItemNew = DialogEditaItemNew(this, itemCampanha)
         dialogEditaItemNew.show(parentFragmentManager, "Dialog edita item")
@@ -168,18 +182,17 @@ class CadastroCampanhaFragment : Fragment(), ItemAdapterOng.ItemListener,
     }
 
     override fun passaItem(item: CampanhaItemDto) {
-        if (!isEdita){
+        if (!isEdita) {
             campanha.itens.add(item)
             atualizaQuantidadeDeItens()
-        }else{
-            isEdita=false
+        } else {
+            isEdita = false
         }
-
         notificaMudancaAdapter()
     }
 
     override fun onClose() {
-        isEdita=false
+        isEdita = false
     }
 
 }
