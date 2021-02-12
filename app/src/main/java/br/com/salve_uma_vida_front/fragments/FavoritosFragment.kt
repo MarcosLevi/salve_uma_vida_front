c1d91260c2af122adaecf5be0a05fa85c4beb2f9
MarcosLevi <marcoslevi1213@gmail.com>
2021-02-11 23:00:58 -0300

    Favoritos fragment mostrando ongs favoritas

--- app/src/main/java/br/com/salve_uma_vida_front/fragments/FavoritosFragment.kt
+++ app/src/main/java/br/com/salve_uma_vida_front/fragments/FavoritosFragment.kt
@@ -5,15 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import br.com.salve_uma_vida_front.R
+import br.com.salve_uma_vida_front.adapters.CardOngFavoritaAdapter
+import br.com.salve_uma_vida_front.closeLoading
 import br.com.salve_uma_vida_front.databinding.FragmentBothFavoritosBinding
 import br.com.salve_uma_vida_front.dto.OngFavoritaDto
+import br.com.salve_uma_vida_front.startLoading
 import br.com.salve_uma_vida_front.toolbarVazia
 import br.com.salve_uma_vida_front.viewholders.CardOngFavoritaViewHolder
+import br.com.salve_uma_vida_front.viewmodels.UserViewModel
 
 class FavoritosFragment : Fragment() {
 
+    private lateinit var viewModel: UserViewModel
     lateinit var mRecyclerView: RecyclerView
     lateinit var mLayoutManager: RecyclerView.LayoutManager
     lateinit var binding: FragmentBothFavoritosBinding
@@ -26,30 +34,48 @@ class FavoritosFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentBothFavoritosBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProviders.of(this).get(UserViewModel::class.java)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        startLoading(requireActivity(), R.id.mainLoading)
         configuraRecyclerView()
         configuraToolbar()
+        configuraObservers()
+        carregaOngsFavoritas()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun carregaOngsFavoritas(){
+        viewModel.getOngsFavororitasDoUserLogado()
+    }
+
+    private fun configuraObservers() {
+        viewModel.ongsFavoritasDoUserLogado.observe(viewLifecycleOwner, Observer {
+            listaOngsFavoritas.clear()
+            if (it != null) {
+                listaOngsFavoritas.addAll(it)
+            }
+            mRecyclerView.adapter!!.notifyDataSetChanged()
+            closeLoading(requireActivity(), R.id.mainLoading)
+        })
     }
 
     private fun configuraToolbar() {
         val toolbar = toolbarVazia(activity)
-//        toolbar?.inflateMenu(R.menu.fragment_both_procurar_menu)
     }
 
     private fun configuraRecyclerView() {
         mRecyclerView = binding.cardsFavoritos
         mRecyclerView.setHasFixedSize(true)
         mLayoutManager = LinearLayoutManager(requireContext())
-//        ongFavoritaAdapter =
-//            CardOngFavoritaAdapter(
-//                listaOngsFavoritas,
-//                requireContext()
-//            )
+        ongFavoritaAdapter =
+            CardOngFavoritaAdapter(
+                listaOngsFavoritas,
+                requireContext()
+            )
         mRecyclerView.layoutManager = mLayoutManager
-//        mRecyclerView.adapter = ongFavoritaAdapter
+        mRecyclerView.adapter = ongFavoritaAdapter
     }
 }
